4d45046d465d13fc1a59b403e2edc4dafb147191
试上高峰窥皓月 <chenxian660@gmail.com>
2022-12-17 14:19:56 +0800

    Update Some

--- project/scripts/autosize-iframes.js
+++ project/scripts/autosize-iframes.js
@@ -3,13 +3,13 @@
  * @param {Element} iframe 需要自适应高度的 iframe
  */
 function changeFrameHeight(iframe) {
-  if (iframe == undefined) return;
-  iframe.height = iframe.contentDocument.body.scrollHeight + 1;
+  if (iframe == undefined) return
+  iframe.height = iframe.contentDocument.body.scrollHeight + 1
 }
 
-let iframes = document.getElementsByClassName("container");
+let iframes = document.getElementsByClassName('container')
 for (const iframe of iframes) {
   iframe.onload = window.onresize = function () {
-    changeFrameHeight(this);
-  };
+    changeFrameHeight(this)
+  }
 }
--- project/scripts/index.js
+++ project/scripts/index.js
@@ -2,35 +2,35 @@
  * 轮播图
  * from:L1 to:L113
  */
-const IMAGES_BOX = document.getElementById("images_box");
-const IMAGES_SIZE = IMAGES_BOX.childElementCount;
-const IMAGE_WIDTH = 1366;
-const MIN_INDEX = 0;
-const MAX_INDEX = IMAGES_SIZE - 1;
-let index = MIN_INDEX;
+const IMAGES_BOX = document.getElementById('images_box')
+const IMAGES_SIZE = IMAGES_BOX.childElementCount
+const IMAGE_WIDTH = 1366
+const MIN_INDEX = 0
+const MAX_INDEX = IMAGES_SIZE - 1
+let index = MIN_INDEX
 
 /**
  * 在移动之前，预先准备图片
  * @param {String} orientation 图片移动方向
  */
-function prepareImages(orientation = "r2l") {
+function prepareImages(orientation = 'r2l') {
   switch (orientation) {
-    case "r2l":
+    case 'r2l':
       // 到达最后一个图片时
       if (index === MAX_INDEX) {
-        IMAGES_BOX.append(IMAGES_BOX.children[0]);
-        index--;
-        IMAGES_BOX.style.left = -index * IMAGE_WIDTH + "px";
+        IMAGES_BOX.append(IMAGES_BOX.children[0])
+        index--
+        IMAGES_BOX.style.left = -index * IMAGE_WIDTH + 'px'
       }
-      break;
-    case "l2r":
+      break
+    case 'l2r':
       // 到达第一个图片时
       if (index === MIN_INDEX) {
         IMAGES_BOX.prepend(
           IMAGES_BOX.children[IMAGES_BOX.childElementCount - 1]
-        );
-        index++;
-        IMAGES_BOX.style.left = -index * IMAGE_WIDTH + "px";
+        )
+        index++
+        IMAGES_BOX.style.left = -index * IMAGE_WIDTH + 'px'
       }
   }
 }
@@ -39,51 +39,51 @@ function prepareImages(orientation = "r2l") {
  * 移动图片
  * @param {String} orientation 图片移动方向
  */
-function move(orientation = "r2l") {
+function move(orientation = 'r2l') {
   switch (orientation) {
     // 图片从右向左移
-    case "r2l":
+    case 'r2l':
       if (IMAGES_BOX.onMoving === undefined) {
         // 准备图片
-        prepareImages(orientation);
+        prepareImages(orientation)
 
         // 开始移动
         IMAGES_BOX.onMoving = window.setInterval(function () {
           // 已经到达指定位置
           if (IMAGES_BOX.offsetLeft <= -(index + 1) * IMAGE_WIDTH) {
-            window.clearInterval(IMAGES_BOX.onMoving);
-            index++;
-            IMAGES_BOX.onMoving = undefined;
+            window.clearInterval(IMAGES_BOX.onMoving)
+            index++
+            IMAGES_BOX.onMoving = undefined
           }
 
           // 还未到达指定位置
           else {
-            IMAGES_BOX.style.left = IMAGES_BOX.offsetLeft - 5 + "px";
+            IMAGES_BOX.style.left = IMAGES_BOX.offsetLeft - 5 + 'px'
           }
-        }, 1);
+        }, 1)
       }
-      break;
+      break
 
     // 图片从左向右移
-    case "l2r":
+    case 'l2r':
       if (IMAGES_BOX.onMoving === undefined) {
         // 准备图片
-        prepareImages(orientation);
+        prepareImages(orientation)
 
         //开始移动
         IMAGES_BOX.onMoving = window.setInterval(function () {
           // 已经到达指定位置
           if (IMAGES_BOX.offsetLeft >= -(index - 1) * IMAGE_WIDTH) {
-            window.clearInterval(IMAGES_BOX.onMoving);
-            index--;
-            IMAGES_BOX.onMoving = undefined;
+            window.clearInterval(IMAGES_BOX.onMoving)
+            index--
+            IMAGES_BOX.onMoving = undefined
           }
 
           // 还未到达指定位置
           else {
-            IMAGES_BOX.style.left = IMAGES_BOX.offsetLeft + 5 + "px";
+            IMAGES_BOX.style.left = IMAGES_BOX.offsetLeft + 5 + 'px'
           }
-        }, 1);
+        }, 1)
       }
   }
 }
@@ -91,23 +91,23 @@ function move(orientation = "r2l") {
 /**
  * 播放上一张图片
  */
-document.getElementById("previous").addEventListener("click", function () {
-  move("l2r");
-});
+document.getElementById('previous').addEventListener('click', function () {
+  move('l2r')
+})
 
 /**
  * 播放下一张图片
  */
-document.getElementById("next").addEventListener("click", function () {
-  move("r2l");
-});
+document.getElementById('next').addEventListener('click', function () {
+  move('r2l')
+})
 
 /**
  * 自动播放轮播图
  */
 const play = function () {
   if (IMAGES_BOX.onPlaying === undefined) {
-    IMAGES_BOX.onPlaying = window.setInterval(move, 4000);
+    IMAGES_BOX.onPlaying = window.setInterval(move, 4000)
   }
-};
-play();
+}
+play()
